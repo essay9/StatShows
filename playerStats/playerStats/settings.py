97977e6c6637cf2cec34442e8494f5d372f98114
essay9 <esai1785@gmail.com>
2024-07-12 21:19:40 -0400

    first commit

--- playerStats/playerStats/settings.py
+++ playerStats/playerStats/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-+9pe3&dcb2aj%gn5nf8bjb+q7y7yahsgq9z%n+*r)s1@1e8f0n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
